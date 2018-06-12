aac856c370fd39e2a8544341ae0a146a9262cf14
Igor Postolenko <igorpost92@yandex.ru>
2018-06-12 20:01:22 +0300

    add downloading source-files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,8 +8,8 @@ import download from '../src/';
 describe('Make name', () => {
   it('set1', () => {
     const address = 'https://hexlet.io/courses';
-    const res = 'hexlet-io-courses';
-    expect(makeName(address)).toBe(res);
+    const expected = 'hexlet-io-courses';
+    expect(makeName(address)).toBe(expected);
   });
 });
 
@@ -32,16 +32,37 @@ describe('Download', () => {
   });
 
   it('set1', async () => {
-    const address = 'https://hexlet.io/courses';
-    const outDir = await mkdtemp();
-    const resultFilepath = path.join(__dirname, '__fixtures__', 'ind.html');
+    const fixtures = path.join(__dirname, '__fixtures__');
 
-    const data = await fs.readFile(resultFilepath, 'utf-8');
-
+    const data = await fs.readFile(path.join(fixtures, 'before.html'), 'utf-8');
     nock('https://hexlet.io')
       .get('/courses')
       .reply(200, data);
 
+    const image = await fs.readFile(path.join(fixtures, 'icon.png'));
+    nock('http://test.ru')
+      .get('/icon.png')
+      .reply(200, image);
+
+    const address = 'https://hexlet.io/courses';
+    const outDir = await mkdtemp();
     await download(address, outDir);
+
+    const expected1 = [
+      'hexlet-io-courses.html',
+      'hexlet-io-courses_files',
+    ];
+    const files1 = await fs.readdir(outDir);
+    expect(files1).toEqual(expected1);
+
+    const expected2 = [
+      'test-ru-icon.png',
+    ];
+    const files2 = await fs.readdir(path.join(outDir, expected1[1]));
+    expect(files2).toEqual(expected2);
+
+    const resultHtml = await fs.readFile(path.join(outDir, 'hexlet-io-courses.html'), 'utf-8');
+    const expectedHtml = await fs.readFile(path.join(fixtures, 'after.html'), 'utf-8');
+    expect(resultHtml).toBe(expectedHtml);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,74 @@
 import { get } from 'axios';
-import { tmpdir } from 'os';
 import path from 'path';
 import fs from 'mz/fs';
+import cheerio from 'cheerio';
 
 import { makeName } from './utils';
 
-const download = (siteUrl, outDir = tmpdir()) => {
+const removeDuplicates = (items) => {
+  const processed = new Set();
+  const uniqs = items.filter(({ link }) => {
+    if (processed.has(link)) {
+      return false;
+    }
+
+    processed.add(link);
+    return true;
+  });
+  return uniqs;
+};
+
+const storeSrcFile = ({ link, saveAs }) => get(link, { responseType: 'arraybuffer' })
+  .then(({ data }) => fs.writeFile(saveAs, data))
+  .catch(() => {
+    // TODO: error during downloading file
+    // https://cdn2.hexlet.io/assets/essential-a6af23ea2acf1f29eccada458f710ccfc9cb1d9d13dad1ab154a54fe65c167ee.js
+  });
+
+const processDocument = (urlLink, html, outDir) => {
+  const name = makeName(urlLink);
+  const relPath = `${name}_files`;
+  const srcDir = path.join(outDir, relPath);
+
+  const $ = cheerio.load(html);
+
+  const processSourceFiles = (tagName, attrName, extraFilter) => {
+    const selection = $(tagName);
+    const tags = extraFilter === undefined ? selection : selection.filter(extraFilter);
+    return tags
+      .map((ind, el) => {
+        const link = $(el).attr(attrName);
+        const ext = path.extname(link);
+        const withoutExt = ext.length ? link.slice(0, -ext.length) : link;
+
+        const filename = `${makeName(withoutExt)}${ext}`;
+
+        const relUrl = path.join(relPath, filename);
+        const saveAs = path.join(srcDir, filename);
+
+        $(el).attr(attrName, relUrl);
+        return { link, saveAs };
+      })
+      .toArray();
+  };
+
+  const scripts = processSourceFiles('script', 'src', (ind, el) => $(el).attr('src') !== undefined);
+  const links = processSourceFiles('link', 'href', (ind, el) => $(el).attr('type') !== 'application/rss+xml');
+  const images = processSourceFiles('img', 'src');
+
+  const files = removeDuplicates([...scripts, ...links, ...images]);
+
+  const result = $.html();
+
+  const pathfile = path.join(outDir, `${name}.html`);
+
+  return fs.access(srcDir)
+    .catch(() => fs.mkdir(srcDir))
+    .then(() => Promise.all(files.map(storeSrcFile)))
+    .then(() => fs.writeFile(pathfile, result));
+};
+
+const download = (siteUrl, outDir = process.cwd()) => {
   if (!siteUrl) {
     throw new Error('Website url is not provided');
   }
@@ -16,19 +79,16 @@ const download = (siteUrl, outDir = tmpdir()) => {
         throw new Error('Provided path is not a directory');
       }
 
-      return get(siteUrl);
+      return get(siteUrl, {
+        responseType: 'arraybuffer',
+      });
     })
     .then((response) => {
       if (response.status !== 200) {
         throw new Error(`Returned code ${response.status}: ${response.statusTest}`);
       }
 
-      const name = makeName(siteUrl);
-      const pathfile = path.join(outDir, `${name}.html`);
-      return fs.writeFile(pathfile, response.data);
-    })
-    .catch((err) => {
-      throw err;
+      return processDocument(siteUrl, response.data, outDir);
     });
 };
 
