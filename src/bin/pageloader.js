5048cfb11b941ebf399475907f8f8b80b60c3098
Igor Postolenko <igorpost92@yandex.ru>
2018-06-16 19:16:26 +0300

    refactoring

--- src/bin/pageloader.js
+++ src/bin/pageloader.js
@@ -4,34 +4,17 @@ import programm from 'commander';
 import { version } from '../../package.json';
 import download from '../';
 
-process.on('uncaughtException', (err) => {
-  console.error(err.message);
-  process.exitCode = 1;
-});
-
-const action = (url, dir) => {
-  // const tasks = new Listr([
-  //   {
-  //     title: 'Saving page',
-  //     task: () => download(url, dir),
-  //   },
-  // ]);
-
-  // tasks.run()
-  //   .catch((err) => {
-  //     console.error(err.message);
-  //     process.exitCode = 2;
-  //   });
-  download(url, dir);
-};
-
 programm
   .version(version)
   .arguments('<url>')
   .description('Downloads a website to your local computer.')
   .option('-o, --output [dir]', 'output directory')
   .action((url) => {
-    action(url, programm.output);
+    download(url, programm.output)
+      .catch((err) => {
+        console.error(err.message);
+        process.exit(2);
+      });
   })
   .parse(process.argv);
 
